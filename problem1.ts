d50e74cb43f92db0051537cfda927afdb464e804
ivan.tarasiuk <ivan.tarasiuk@leverx.com>
2024-09-16 13:02:24 +0200

    feat: create solution for problem1

---
+++ problem1.ts
@@ -0,0 +1,26 @@
+// type partFuncOutput = (array: Array<number>) => number | number;
+
+export function sum(args: Array<number>): number {
+  // console.log(args)
+  return args.reduce((akum, next) => akum + next, 0);
+}
+
+export function partFunc(
+  fn: (array: Array<number>) => number,
+  ...argsInput: number[]
+) {
+  // console.log(argsInput);
+  if (argsInput.length !== 0) {
+    return (...argsInside: number[]): number => {
+      return fn(argsInput.concat(...argsInside));
+    };
+  }
+  return null;
+}
+
+// const partSum = partFunc(sum, 3, 4, 5);
+// const partSum0 = partFunc(sum);
+// console.log(partSum(23));
+// console.log(partSum0());
+// const partSum0 = partFunc(sum);
+// console.log(partSum0);
