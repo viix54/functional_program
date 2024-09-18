475abe64ee72a81cc46c669fcc6fc2f1bcba9c3d
ivan.tarasiuk <ivan.tarasiuk@leverx.com>
2024-09-18 17:47:56 +0200

    fix: update  mapTwiceBiggerEl.spec.ts test file by adding an empty array calling map function check

--- __test__/mapTwiceBiggerEl.spec.ts
+++ __test__/mapTwiceBiggerEl.spec.ts
@@ -7,4 +7,8 @@ describe(`Testing map implementation functionality`, () => {
   it(`return array with twice bigger elements value`, () => {
     expect(sut([1, 2, 3, 4])).toStrictEqual([2, 4, 6, 8]);
   });
+
+  it(`return an empty array when map function was called with array without elements`, () => {
+    expect(sut([])).toStrictEqual([]);
+  });
 });
